ae4eee4077e5d821869d1453e3575b97c8374589
Odin <odinuv@gmail.com>
2016-09-14 09:59:53 +0200

    fix: user add

--- userconf.sh
+++ userconf.sh
@@ -1,41 +1,18 @@
 #!/usr/bin/with-contenv bash
 
-## Set defaults for environmental variables in case they are undefined
-USER=${USER:=rstudio}
-PASSWORD=${PASSWORD:=rstudio}
-USERID=${USERID:=1000}
-ROOT=${ROOT:=FALSE}
-
-if [ "$USERID" -ne 1000 ]
-## Configure user with a different USERID if requested.
-	then
-		echo "deleting user rstudio"
-		userdel rstudio
-		echo "creating new $USER with UID $USERID"
-		useradd -m $USER -u $USERID
-		mkdir /home/$USER
-		chown -R $USER /home/$USER
-elif [ "$USER" != "rstudio" ]
-	then
-		## cannot move home folder when it's a shared volume, have to copy and change permissions instead
-		cp -r /home/rstudio /home/$USER
-		## RENAME the user 	
-		usermod -l $USER -d /home/$USER rstudio
-		groupmod -n $USER rstudio 
-		chown -R $USER:$USER /home/$USER
-		echo "USER is now $USER"	
+# Environment variables must be provided
+if [ -z ${USER+x} ] || [ -z ${PASSWORD+x} ] ; then
+	echo "USER and PASSWORD must be provided"
 fi
-	
-## Add a password to user
+
+# Create a group for the RStudioServer and grant access to $R_HOME/etc/		 
+groupadd rserver
+chgrp rserver -R $R_HOME/etc/		
+  		  
+useradd $USER
+usermod -a -G root $USER
 echo "$USER:$PASSWORD" | chpasswd
 
-# Use Env flag to know if user should be added to sudoers
-if [ "$ROOT" == "TRUE" ]
-	then
-		adduser $USER sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-		echo "$USER added to sudoers"
-fi
-
 ## add these to the global environment so they are avialable to the RStudio user 
-echo "HTTR_LOCALHOST=$HTTR_LOCALHOST" >> /etc/R/Renviron.site
-echo "HTTR_PORT=$HTTR_PORT" >> /etc/R/Renviron.site
+echo "HTTR_LOCALHOST=$HTTR_LOCALHOST" >> $R_HOME/etc/Renviron.site
+echo "HTTR_PORT=$HTTR_PORT" >> $R_HOME/etc/Renviron.site
