5d400e9745faf18a86f1d71dac265f47e9dfa809
Odin <odinuv@gmail.com>
2016-09-14 23:17:36 +0200

    fix: don't continue on error

--- userconf.sh
+++ userconf.sh
@@ -3,6 +3,7 @@
 # Environment variables must be provided
 if [ -z ${USER+x} ] || [ -z ${PASSWORD+x} ] ; then
 	echo "USER and PASSWORD must be provided"
+	exit 2
 fi
 
 # Create a group for the RStudioServer and grant access to $R_HOME/etc/		 
