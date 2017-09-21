f9941952527119dfc0c134a2ea3c1c4ac480dc6e
Ondrej Popelka <odinuv@gmail.com>
2017-09-21 21:35:47 +0200

    refactor: remove unnecessary installs to speedup build

--- code/init.R
+++ code/init.R
@@ -1,13 +1,4 @@
-# install really required packages
-install.packages(
-	c('readr', 'devtools'), 
-	dependencies = c("Depends", "Imports", "LinkingTo"), 
-	INSTALL_opts = c("--no-html")
-)
-
-library('devtools')
 
 # install the transformation application ancestors
-devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
-devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
-devtools::install_github('keboola/r-transformation', ref = "1.1.0", force = TRUE)
+devtools::install_github('keboola/r-docker-application', ref = "1.0.2")
+devtools::install_github('keboola/r-transformation', ref = "1.1.2")
