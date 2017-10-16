66f61086cbda6d2e5e3f8329a505f1b51bbf28b8
Odin <odinuv@gmail.com>
2017-10-16 18:05:43 +0200

    fix: support rgl packages

--- code/rsession_init.R
+++ code/rsession_init.R
@@ -1,23 +1,23 @@
 tryCatch({
-	rscript <- Sys.getenv('SCRIPT')
-	# because EOS is null, rscript mustn't be empty
-	if (nchar(rscript) == 0) {
-		if (file.exists('/data/main.R')) {
-			rscript = readChar('/data/main.R', file.info('/data/main.R')$size)
-		} else {
-			rscript = ' '
-		}
-	}
-	fileName <- '/code/templatefile.json'
-	data <- readChar(fileName, file.info(fileName)$size)
-	configData <- jsonlite::fromJSON(data)
-	configData$contents <- rscript
-	jsonData <- jsonlite::toJSON(configData, auto_unbox = TRUE, pretty = TRUE)
-	writeChar(jsonData, paste0('/data/.rstudio/sdb/per/t/AAAAAAA'), eos = NULL)
-	writeChar(rscript, '/data/main.R', eos = NULL)
+    rscript <- Sys.getenv('SCRIPT')
+    # because EOS is null, rscript mustn't be empty
+    if (nchar(rscript) == 0) {
+        if (file.exists('/data/main.R')) {
+            rscript = readChar('/data/main.R', file.info('/data/main.R')$size)
+        } else {
+            rscript = ' '
+        }
+    }
+    fileName <- '/code/templatefile.json'
+    data <- readChar(fileName, file.info(fileName)$size)
+    configData <- jsonlite::fromJSON(data)
+    configData$contents <- rscript
+    jsonData <- jsonlite::toJSON(configData, auto_unbox = TRUE, pretty = TRUE)
+    writeChar(jsonData, paste0('/data/.rstudio/sdb/per/t/AAAAAAA'), eos = NULL)
+    writeChar(rscript, '/data/main.R', eos = NULL)
 }, error = function(e) {
-	print("Failed to load script.")
-	quit(151)
+    print("Failed to load script.")
+    quit(151)
 })
 
 library('keboola.r.transformation')
@@ -25,34 +25,34 @@ app <- RTransformation$new('/data/')
 
 packages <- Sys.getenv('PACKAGES')
 if (packages != "") {
-	print(paste0("Processing packages from:", packages))
-	app$packages <- tryCatch({
-		as.character(jsonlite::fromJSON(packages))
-	}, error = function(e) {
-		print(paste0("Packages is not a JSON array ", packages))
-		quit(save = 'no', status = 152, runLast = FALSE)
-	})
-	tryCatch({
-		app$installModulePackages()
-	}, error = function(e) {
-		print(paste0("Faield to install packages ", e))
-		quit(save = 'no', status = 153, runLast = FALSE)
-	})
+    print(paste0("Processing packages from:", packages))
+    app$packages <- tryCatch({
+        as.character(jsonlite::fromJSON(packages))
+    }, error = function(e) {
+        print(paste0("Packages is not a JSON array ", packages))
+        quit(save = 'no', status = 152, runLast = FALSE)
+    })
+    tryCatch({
+        app$installModulePackages()
+    }, error = function(e) {
+        print(paste0("Failed to install packages ", e))
+        quit(save = 'no', status = 153, runLast = FALSE)
+    })
 }
 
 tags <- Sys.getenv('TAGS')
 if (tags != "") {
-	print(paste0("Processing tagged files from:", tags))
-	app$tags <- tryCatch({
-		as.character(jsonlite::fromJSON(tags))
-	}, error = function(e) {
-		print(paste0("Tags is not a JSON array ", tags))
-		quit(save = 'no', status = 154, runLast = FALSE)
-	})
-	tryCatch({
-		app$prepareTaggedFiles()
-	}, error = function(e) {
-		print(paste0("Failed to prepare files ", e))
-		quit(save = 'no', status = 155, runLast = FALSE)
-	})
+    print(paste0("Processing tagged files from:", tags))
+    app$tags <- tryCatch({
+        as.character(jsonlite::fromJSON(tags))
+    }, error = function(e) {
+        print(paste0("Tags is not a JSON array ", tags))
+        quit(save = 'no', status = 154, runLast = FALSE)
+    })
+    tryCatch({
+        app$prepareTaggedFiles()
+    }, error = function(e) {
+        print(paste0("Failed to prepare files ", e))
+        quit(save = 'no', status = 155, runLast = FALSE)
+    })
 }
