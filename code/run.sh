cee5954b90459adcdf3da3b944cf4944f0989c9e
pivnicek <marc@keboola.com>
2019-11-20 19:31:50 +0100

    switch order of execution

--- code/run.sh
+++ code/run.sh
@@ -7,8 +7,6 @@ if [ -z ${USER} ] || [ -z ${PASSWORD} ] ; then
 	exit 2
 fi
 
-/tmp-rstudio/wait-for-it.sh -t 0 data-loader:80 -- echo "Data loader is up"
-
 useradd $USER
 # add user to the users group (GID 100)
 usermod -a -G users $USER
@@ -20,10 +18,13 @@ echo "$USER:$PASSWORD" | chpasswd
 mkdir -p /data/.rstudio/sdb/per/t/
 chmod a+rwx -R /tmp/
 chmod a+rwx -R /data/
-Rscript /tmp-rstudio/rsession_init.R
 chmod a+rw /data/main.R
 chown -R $USER /data/.rstudio
 chown $USER /data/main.R
 chgrp -R users /data
+
+/tmp-rstudio/wait-for-it.sh -t 0 data-loader:80 -- echo "Data loader is up"
+
+Rscript /tmp-rstudio/rsession_init.R
 
 exec /usr/lib/rstudio-server/bin/rserver --server-daemonize 0
